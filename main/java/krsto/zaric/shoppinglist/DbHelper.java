4318981b094b41b3142ee00e9427e668f27cc94f
zarick1 <krstozaric01@gmail.com>
2025-05-03 15:22:31 +0200

    comments

--- main/java/krsto/zaric/shoppinglist/DbHelper.java
+++ main/java/krsto/zaric/shoppinglist/DbHelper.java
@@ -12,31 +12,62 @@ import androidx.annotation.Nullable;
 
 import java.util.UUID;
 
+/**
+ * Database helper class for managing SQLite database operations in the Shopping List application.
+ * Handles creation, insertion, reading, updating, and deletion of users, lists, and items.
+ *
+ * @author Krsto Zaric
+ */
 public class DbHelper extends SQLiteOpenHelper {
 
+    /** Name of the SQLite database file. */
     public static final String DATABASE_NAME = "shared_list_app.db";
+    /** Version of the database schema. */
     public static final int DATABASE_VERSION = 1;
 
+    /** Name of the table for storing shopping lists. */
     public static final String lists_table_name = "LISTS";
+    /** Column for the list name in the LISTS table. */
     public static final String lists_column_list_name = "listName";
+    /** Column for the list creator in the LISTS table. */
     public static final String lists_column_list_creator = "listCreator";
+    /** Column indicating if the list is shared in the LISTS table. */
     public static final String lists_column_list_shared = "listShared";
 
+    /** Name of the table for storing user data. */
     public static final String users_table_name = "USERS";
+    /** Column for the username in the USERS table. */
     public static final String users_column_username = "username";
+    /** Column for the user email in the USERS table. */
     public static final String users_column_email = "email";
+    /** Column for the user password in the USERS table. */
     public static final String users_column_password = "password";
 
+    /** Name of the table for storing list items. */
     public static final String items_table_name = "ITEMS";
+    /** Column for the item name in the ITEMS table. */
     public static final String items_column_name = "name";
+    /** Column for the associated list name in the ITEMS table. */
     public static final String items_column_list_name = "listName";
+    /** Column indicating if the item is checked in the ITEMS table. */
     public static final String items_column_checked = "checked";
+    /** Column for the unique item ID in the ITEMS table. */
     public static final String items_column_id = "ID";
 
+    /**
+     * Constructs a new DbHelper instance.
+     *
+     * @param context The application context, used to access the database.
+     */
     public DbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Creates the database tables for users, lists, and items when the database is first created.
+     *
+     * @param sqLiteDatabase The SQLite database instance.
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE " + items_table_name + "(" +
@@ -56,12 +87,26 @@ public class DbHelper extends SQLiteOpenHelper {
                 lists_column_list_shared + " TEXT);");
     }
 
+    /**
+     * Handles database schema upgrades. Currently empty as no upgrades are defined.
+     *
+     * @param sqLiteDatabase The SQLite database instance.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        // No upgrade logic implemented
     }
 
-    public long insertList (CharacterModel cm, String username) {
+    /**
+     * Inserts a new shopping list into the LISTS table.
+     *
+     * @param cm The CharacterModel containing list data (name and shared status).
+     * @param username The username of the list creator.
+     * @return The row ID of the inserted list, or -1 if insertion fails.
+     */
+    public long insertList(CharacterModel cm, String username) {
         SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -75,7 +120,15 @@ public class DbHelper extends SQLiteOpenHelper {
         return rowInserted;
     }
 
-    public long insertUser (String username, String email, String password) {
+    /**
+     * Inserts a new user into the USERS table.
+     *
+     * @param username The username of the user.
+     * @param email The email address of the user.
+     * @param password The password of the user.
+     * @return The row ID of the inserted user, or -1 if insertion fails.
+     */
+    public long insertUser(String username, String email, String password) {
         SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -89,7 +142,14 @@ public class DbHelper extends SQLiteOpenHelper {
         return rowInserted;
     }
 
-    public long insertTask (CharacterModel cm, String list) {
+    /**
+     * Inserts a new task (item) into the ITEMS table.
+     *
+     * @param cm The CharacterModel containing task data (name, checked status, and ID).
+     * @param list The name of the list to which the task belongs.
+     * @return The row ID of the inserted task, or -1 if insertion fails.
+     */
+    public long insertTask(CharacterModel cm, String list) {
         SQLiteDatabase db = getWritableDatabase();
 
         Cursor cursor = db.query(items_table_name, null, null, null, null, null, null);
@@ -105,7 +165,13 @@ public class DbHelper extends SQLiteOpenHelper {
         return rowInserted;
     }
 
-    public String readListCreator (String listName) {
+    /**
+     * Retrieves the creator of a specific shopping list.
+     *
+     * @param listName The name of the list.
+     * @return The username of the list creator, or throws an exception if not found.
+     */
+    public String readListCreator(String listName) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(lists_table_name, null, lists_column_list_name + "=?", new String[] { listName }, null, null, null);
@@ -115,7 +181,13 @@ public class DbHelper extends SQLiteOpenHelper {
         return cursor.getString(cursor.getColumnIndexOrThrow(lists_column_list_creator));
     }
 
-    public CharacterModel[] readLists (String username) {
+    /**
+     * Retrieves all lists accessible to a user (created or shared).
+     *
+     * @param username The username of the user.
+     * @return An array of CharacterModel objects representing the lists, or null if none exist.
+     */
+    public CharacterModel[] readLists(String username) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(lists_table_name, null, lists_column_list_creator + "=? OR " + lists_column_list_shared + "=?", new String[] {username, "true"}, null, null, null);
@@ -132,7 +204,13 @@ public class DbHelper extends SQLiteOpenHelper {
         return lists;
     }
 
-    public CharacterModel[] readMyLists (String username) {
+    /**
+     * Retrieves lists created by a specific user.
+     *
+     * @param username The username of the user.
+     * @return An array of CharacterModel objects representing the user's lists, or null if none exist.
+     */
+    public CharacterModel[] readMyLists(String username) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(lists_table_name, null, lists_column_list_creator + "=?", new String[] {username}, null, null, null);
@@ -149,7 +227,14 @@ public class DbHelper extends SQLiteOpenHelper {
         return lists;
     }
 
-    public boolean readList (String name, String creator) {
+    /**
+     * Checks if a specific list exists for a given creator.
+     *
+     * @param name The name of the list.
+     * @param creator The username of the list creator.
+     * @return True if the list exists, false otherwise.
+     */
+    public boolean readList(String name, String creator) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(lists_table_name, null, lists_column_list_name + "=? AND " + lists_column_list_creator + "=?", new String[] { name, creator }, null, null, null);
@@ -160,7 +245,14 @@ public class DbHelper extends SQLiteOpenHelper {
         return true;
     }
 
-    public boolean readUser (String username, String password) {
+    /**
+     * Validates a user's credentials.
+     *
+     * @param username The username to check.
+     * @param password The password to check.
+     * @return True if the credentials are valid, false otherwise.
+     */
+    public boolean readUser(String username, String password) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(users_table_name, null, users_column_username + "=? AND " + users_column_password + "=?", new String[] { username, password }, null, null, null);
@@ -171,7 +263,13 @@ public class DbHelper extends SQLiteOpenHelper {
         return true;
     }
 
-    public boolean readTask (String id) {
+    /**
+     * Checks if a task exists by its ID.
+     *
+     * @param id The unique ID of the task.
+     * @return True if the task exists, false otherwise.
+     */
+    public boolean readTask(String id) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(items_table_name, null, items_column_id + "=?", new String[] { id }, null, null, null);
@@ -182,27 +280,13 @@ public class DbHelper extends SQLiteOpenHelper {
         return true;
     }
 
-//    // ======================
-//    public boolean readUserFilip () {
-//        SQLiteDatabase db = getReadableDatabase();
-//
-//        Cursor cursor = db.query(users_table_name, null, users_column_username + "=?", new String[] { "Filip" }, null, null, null);
-//        if(cursor.getCount() <= 0) {
-//            Log.d("myTag", "No user found");
-//            return false;
-//        }else {
-//            cursor.moveToFirst();
-//            Log.d("myTag", cursor.getString(cursor.getColumnIndexOrThrow(users_column_username)));
-//            Log.d("myTag", cursor.getString(cursor.getColumnIndexOrThrow(users_column_password)));
-//            Log.d("myTag", cursor.getString(cursor.getColumnIndexOrThrow(users_column_email)));
-//        }
-//
-//        close();
-//        return true;
-//    }
-//    // ======================
-
-    public CharacterModel[] readItems (String list) {
+    /**
+     * Retrieves all items for a specific shopping list.
+     *
+     * @param list The name of the list.
+     * @return An array of CharacterModel objects representing the items, or null if none exist.
+     */
+    public CharacterModel[] readItems(String list) {
         SQLiteDatabase db = getReadableDatabase();
 
         Cursor cursor = db.query(items_table_name, null, items_column_list_name + "=?", new String[] {list}, null, null, null);
@@ -219,14 +303,20 @@ public class DbHelper extends SQLiteOpenHelper {
         return lists;
     }
 
+    /**
+     * Updates the checked status of a task in the ITEMS table.
+     *
+     * @param taskId The unique ID of the task.
+     * @param checked The new checked status ("true" or "false").
+     */
     public void updateTask(String taskId, String checked) {
         SQLiteDatabase db = getWritableDatabase();
 
         Cursor cursor = db.query(items_table_name, null, items_column_id + "=?", new String[] {taskId}, null, null, null);
         cursor.moveToFirst();
         String taskName = cursor.getString(cursor.getColumnIndexOrThrow(items_column_name));
-        String taskListName =  cursor.getString(cursor.getColumnIndexOrThrow(items_column_list_name));
-        String id =  cursor.getString(cursor.getColumnIndexOrThrow(items_column_id));
+        String taskListName = cursor.getString(cursor.getColumnIndexOrThrow(items_column_list_name));
+        String id = cursor.getString(cursor.getColumnIndexOrThrow(items_column_id));
 
         ContentValues cv = new ContentValues();
         cv.put(items_column_name, taskName);
@@ -238,30 +328,51 @@ public class DbHelper extends SQLiteOpenHelper {
         close();
     }
 
-    public void deleteList (String ln) {
+    /**
+     * Deletes a shopping list and its associated items from the database.
+     *
+     * @param ln The name of the list to delete.
+     */
+    public void deleteList(String ln) {
         SQLiteDatabase db = getWritableDatabase();
         db.delete(lists_table_name, lists_column_list_name + "=?", new String[] { ln });
         db.delete(items_table_name, items_column_list_name + "=?", new String[] { ln });
         close();
     }
 
-    public void deleteTask (String ln) {
+    /**
+     * Deletes a specific task from the ITEMS table.
+     *
+     * @param ln The unique ID of the task to delete.
+     */
+    public void deleteTask(String ln) {
         SQLiteDatabase db = getWritableDatabase();
-        db.delete(items_table_name, items_column_id + " =?", new String[] { ln });
+        db.delete(items_table_name, items_column_id + "=?", new String[] { ln });
         close();
     }
 
-    private CharacterModel createList (Cursor cursor) {
+    /**
+     * Creates a CharacterModel for a shopping list from a database cursor.
+     *
+     * @param cursor The cursor containing list data.
+     * @return A CharacterModel representing the list.
+     */
+    private CharacterModel createList(Cursor cursor) {
         String listName = cursor.getString(cursor.getColumnIndexOrThrow(lists_column_list_name));
         String listShared = cursor.getString(cursor.getColumnIndexOrThrow(lists_column_list_shared));
         return new CharacterModel(listName, Boolean.valueOf(listShared), null);
     }
 
-    private CharacterModel createTask (Cursor cursor) {
+    /**
+     * Creates a CharacterModel for a task from a database cursor.
+     *
+     * @param cursor The cursor containing task data.
+     * @return A CharacterModel representing the task.
+     */
+    private CharacterModel createTask(Cursor cursor) {
         String taskName = cursor.getString(cursor.getColumnIndexOrThrow(items_column_name));
         String taskShared = cursor.getString(cursor.getColumnIndexOrThrow(items_column_checked));
         String taskId = cursor.getString(cursor.getColumnIndexOrThrow(items_column_id));
         return new CharacterModel(taskName, Boolean.valueOf(taskShared), taskId);
     }
-
 }
